008c31eb1bcf0929c811b5c2a7865a0261d536d4
Dan O'Connor <dpo3ys@virginia.edu>
2019-03-27 21:43:40 -0400

    Adding missing files

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject PlaneEnemy;
+
+    public int NumEnemies;
+    public float RangeToSpawnEnemies;
+
+    void Start()
+    {
+        _player = Player.GetComponent<Rigidbody2D>();
+        _enemyPositions = new List<Vector2>();
+    }
+
+    void FixedUpdate()
+    {
+        spawnObjects();
+    }
+
+    private void spawnObjects()
+    {
+        // Need a given amount of enemies above, below, left, and right of the player
+        // If there are not enough enemies in a given sector, spawn more now
+
+        Vector2 playerPos = _player.position;
+
+        int aboveCount = _enemyPositions.Count(pos => pos.y > playerPos.y && pos.y - playerPos.y < RangeToSpawnEnemies);
+        for (int i = aboveCount; i < NumEnemies; i++)
+        {
+            Vector2 spawnPos = new Vector2(Random.Range(playerPos.x - RangeToSpawnEnemies, playerPos.x + RangeToSpawnEnemies),
+                                            Random.Range(playerPos.y, playerPos.y + RangeToSpawnEnemies));
+            _enemyPositions.Add(spawnPos);
+            Instantiate(PlaneEnemy, spawnPos, Quaternion.identity);
+        }
+    }
+
+    private Rigidbody2D _player;
+    private List<Vector2> _enemyPositions;
+
+    private const string _playerTag = "Player";
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float BoostAcceleration;
+    public float RotationRate;
+    public float MaxSpeed;
+
+    void Start()
+    {
+        _player = GetComponent<Rigidbody2D>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _sprites = Resources.LoadAll<Sprite>(SpriteName);
+    }
+
+    void FixedUpdate()
+    {
+        // Change rotation, then add new direction to velocity
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            _player.rotation += RotationRate;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            _player.rotation -= RotationRate;
+        }
+        
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            float rotationInRads = _player.rotation * Mathf.Deg2Rad;
+            Vector2 direction = new Vector2(-1 * Mathf.Sin(rotationInRads), Mathf.Cos(rotationInRads));
+
+            Vector2 newVelocity = _player.velocity + direction;
+
+            // Only apply speed cap if the player is moving upwards and is greater than the max speed
+            if (newVelocity.y > MaxSpeed && newVelocity.magnitude > MaxSpeed)
+            {
+                newVelocity = newVelocity.normalized * MaxSpeed;
+            }
+
+            _player.velocity = newVelocity;
+
+            _spriteRenderer.sprite = _sprites[1];
+        }
+        else
+        {
+            _spriteRenderer.sprite = _sprites[0];
+        }
+    }
+
+    private Rigidbody2D _player;
+    private SpriteRenderer _spriteRenderer;
+    private Sprite[] _sprites;
+
+    private float _maxSpeed;
+
+    private const string SpriteName = "sprites/player";
+}
